89e626a344e77cfa0adbf47e4c3505866d559e35
Brian Hobbs <brianhobbs216@gmail.com>
2022-06-15 16:17:13 -0400

    Add home hero

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,43 +1,33 @@
-import React from 'react';
+import React from "react";
 import Header from "../../components/Header/Header";
-import {getProviders, signIn} from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
-import Image from "next/image";
-
-function Login({providers}) {
-    return (
-        <div>
-              <Header />
-              <div className="container-fluid mx-auto">
-                  <div className="container mx-auto flex justify-center">
-
-                 
-                    {Object.values(providers).map(provider => (
-                        <div key={provider.name} className="mt-20 flex flex-col justify-center">
-                        
-                            <img className="w-[300px] h-[200px]" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Google_logo_%282010-2013%29.svg/2560px-Google_logo_%282010-2013%29.svg.png"
-                               
-                            />
-                            <button 
-                                className="bg-gray-300 rounded-md px-3 py-3"
-                                onClick={() => signIn(provider.id, { callBackUrl: "/" }) }
-                            >
-                                Sigin with google
-                            </button>
-                        </div>
-                    ))}
-              </div>
-                </div>
-        </div>
-    );
+export default function signin({ providers }) {
+  return (
+    <>
+      <Header />
+      <div className="mt-40">
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name} className="flex flex-col items-center">
+            <img
+            className="w-52 object-cover"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+              alt="google-logo"
+            />
+            <p className="text-sm italic my-10 text-center">This is a Google Clone</p>
+            <button className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500" onClick={() => signIn(provider.id, { callbackUrl: "/" })}>
+              Sign in with {provider.name}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export async function getServerSideProps() {
-    const providers = await getProviders();
-
-    return {
-        props: { providers },
-    };
+  const providers = await getProviders();
+  return {
+    props: { providers },
+  };
 }
-
-export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
 import Head from 'next/head';
 import Header from "../components/Header/Header";
+import Image from "next/image"
+import {AiOutlineSearch} from 'react-icons/ai';
+import {FaMicrophone} from 'react-icons/fa';
 
 export default function Home() {
     return (
-        <div className="container-fluid mx-auto">
+        <div className="container-fluid mx-auto  justify-center">
             <Head>
                 <title>Coup Search</title>
                 <meta name="description" content="A Google clone with Next.js" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <Header />
-            <main className="container mx-auto">
-                <h1 className="text-3xl font-bold underline">
-                    Coup Search
-                </h1>
-            </main>
+            <form className="flex flex-col items-center mt-40">
+                <Image
+                    width="300"
+                    objectFit="cover"
+                    height="100"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+                />
+                <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gary-200 hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl">
+                    <AiOutlineSearch className="h-5 text-gray-500 mr-3" />
+                    <input
+                        // ref={searchInputRef}
+                        type="text"
+                        className="flex-grow focus:outline-none"
+                    />
+                    <FaMicrophone className="h-5" />
+                </div>
+                <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
+                    <button className="btn">
+                        Google Search
+                    </button>
+                    <button className="btn">
+                        I&apos;m Feeling Lucky
+                    </button>
+                </div>
+            </form>
         </div>
+             
+  
     )
 }
